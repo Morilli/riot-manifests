ac9ff0db963c0f52a8edd52993013e6f3b9625f6
Morilli <35152647+Morilli@users.noreply.github.com>
2020-06-03 09:15:03 +0200

    Update valorant.py for public release

--- VALORANT.py
+++ VALORANT.py
@@ -1,18 +1,10 @@
-from login import get_tokens
-import sys
 import requests
 import json
 import os
 
-if (len(sys.argv) < 3):
-    print("Error: Provide username and password in call.")
-    exit(1)
+valorant_release = requests.get("https://clientconfig.rpg.riotgames.com/api/v1/config/public?namespace=keystone.products.valorant.patchlines", timeout=10)
 
-entitlements_token, bearer = get_tokens(sys.argv[1], sys.argv[2])
-
-player_json = requests.get("https://clientconfig.rpg.riotgames.com/api/v1/config/player?os=windows&region=EUW&app=Riot%20Client&version=99.0.0.9999999&patchline=KeystoneFoundationLiveWin", headers={"X-Riot-Entitlements-JWT": entitlements_token, "Authorization": f"Bearer {bearer}"}, timeout=10)
-
-for configuration in json.loads(player_json.content)["keystone.products.valorant.patchlines.live"]["platforms"]["win"]["configurations"]:
+for configuration in json.loads(valorant_release.content)["keystone.products.valorant.patchlines.live"]["platforms"]["win"]["configurations"]:
     patch_url = configuration["patch_url"]
     id = patch_url[-25:-9]
     region = configuration["valid_shards"]["live"][0]
