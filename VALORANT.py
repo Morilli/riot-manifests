dec5a543f94d3425b0de835cc6db98402da2c8a8
Morilli <35152647+Morilli@users.noreply.github.com>
2020-09-17 03:04:16 +0200

    Update valorant.py to use the exe version embedded in VALORANT-Win64-Shipping.exe
    credits to Uli and the cdragon server for the logic and code and stuff

--- VALORANT.py
+++ VALORANT.py
@@ -1,16 +1,34 @@
 import requests
 import json
 import os
+import re
+import subprocess
+import shutil
+
+def get_valorant_version(path):
+    with open(path, "rb") as exe_file:
+        data = exe_file.read()
+        pattern = "++Ares-Core+release-".encode("utf-16le")
+        pos = data.find(pattern) + len(pattern) + 48
+        return data[pos:pos+32].decode("utf-16le")
 
 valorant_release = requests.get("https://clientconfig.rpg.riotgames.com/api/v1/config/public?namespace=keystone.products.valorant.patchlines", timeout=1)
 
 for configuration in json.loads(valorant_release.content)["keystone.products.valorant.patchlines.live"]["platforms"]["win"]["configurations"]:
     patch_url = configuration["patch_url"]
-    id = patch_url[-25:-9]
     region = configuration["valid_shards"]["live"][0]
     os.makedirs(f"VALORANT/{region}", exist_ok=True)
     try:
-        with open(f"VALORANT/{region}/{id}.txt", "x") as out_file:
+        subprocess.check_call(["./ManifestDownloader.exe", patch_url, "-b", "https://valorant.secure.dyn.riotcdn.net/channels/public/bundles", "-f", "ShooterGame/Binaries/Win64/VALORANT-Win64-Shipping.exe", "-o", "VALORANT/temp", "-t", "4"], timeout=30)
+    except:
+        shutil.rmtree("VALORANT/temp")
+        raise
+    exe_version = get_valorant_version("VALORANT/temp/ShooterGame/Binaries/Win64/VALORANT-Win64-Shipping.exe")
+
+    try:
+        with open(f"VALORANT/{region}/{exe_version}.txt", "x") as out_file:
             out_file.write(patch_url)
     except FileExistsError:
         pass
+
+shutil.rmtree("VALORANT/temp")
