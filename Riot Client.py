168b117ee16a236089e83c601902f354b7f3acec
Morilli <35152647+Morilli@users.noreply.github.com>
2023-11-23 03:06:05 +0100

    don't download unnecessary mac riot client files

--- Riot Client.py
+++ Riot Client.py
@@ -14,11 +14,11 @@ for patchline in patchlines:
     level = json.loads(json_file.content)["keystone.self_update.level"]
     manifest_url = json.loads(json_file.content)["keystone.self_update.manifest_url"]
     os.makedirs(f"Riot Client/{patchline}", exist_ok=True)
-    download_info = ("Contents/Info.plist", "mac") if "Mac" in patchline else ("RiotClientFoundation.dll", "win")
+    download_info = ("^Contents/Info.plist", "mac") if "Mac" in patchline else ("RiotClientFoundation.dll", "win")
     subprocess.check_call(["./ManifestDownloader.exe", manifest_url, "-b", "https://ks-foundation.secure.dyn.riotcdn.net/channels/public/bundles", "-f", download_info[0], "-o", "Riot Client/temp", "-t", "8"], timeout=10)
     if download_info[1] == "win":
         exe_version = get_exe_version(f"Riot Client/temp/{download_info[0]}")
     else:
-        with open(f"Riot Client/temp/{download_info[0]}", "rb") as in_file:
+        with open(f"Riot Client/temp/{download_info[0][1:]}", "rb") as in_file:
             exe_version = f'{plistlib.load(in_file)["FileVersion"]}_{manifest_url[-25:-9]}'
     save_file(f"Riot Client/{patchline}/{exe_version}_{level}.json", json_file.content)
