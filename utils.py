50a6255a0e627090bd6820054dca7250180e947d
Morilli <35152647+Morilli@users.noreply.github.com>
2024-05-24 07:10:47 +0200

    add ME1 region

--- utils.py
+++ utils.py
@@ -79,7 +79,7 @@ def download_manifest(manifest_url, temp_path, session=None):
     if os.path.exists(manifest_path):
         return
 
-    manifest = session.get(manifest_url, timeout=1)
+    manifest = session.get(manifest_url, timeout=2)
     manifest.raise_for_status()
 
     with open(f"{manifest_path}.temp", "wb") as out_file:
