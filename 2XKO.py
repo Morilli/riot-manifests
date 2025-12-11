c393c3298d8eea37f4c293ad8c754c6ffc10eaef
Morilli <35152647+Morilli@users.noreply.github.com>
2025-12-11 01:58:45 +0100

    adjust 2XKO script to catch playstation/xbox hotfixes

--- 2XKO.py
+++ 2XKO.py
@@ -8,7 +8,8 @@ def update_versions():
     for release in lion_releases.json()["releases"]:
         artifact_type_id = release["release"]["labels"]["riot:artifact_type_id"]["values"][0]
         artifact_version_id = release["release"]["labels"]["riot:artifact_version_id"]["values"][0]
-        path = f'2XKO/{artifact_type_id}'
+        platform = release["release"]["labels"]["riot:platform"]["values"][0]
+        path = f'2XKO/{artifact_type_id}/{platform}' if platform != "windows" else f'2XKO/{artifact_type_id}'
 
         filename = f"{artifact_version_id}.txt" if artifact_type_id == "base_release" else f"{release["release"]["labels"]["Stream"]["values"][0]}_{artifact_version_id}.txt"
         os.makedirs(path, exist_ok=True)
