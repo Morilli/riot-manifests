c42f38d20fe8b6a258d4ab5054e65252a5027daf
Morilli <35152647+Morilli@users.noreply.github.com>
2026-06-02 20:48:12 +0200

    me when riot:

--- Riot Client.py
+++ Riot Client.py
@@ -2,6 +2,7 @@ from utils import get_exe_version, save_file, setup_session
 import os
 import subprocess
 import plistlib
+import json
 
 patchlines = ["KeystoneFoundationLiveWin", "KeystoneFoundationBetaWin", "KeystoneFoundationLiveMac", "KeystoneFoundationBetaMac"]
 session = setup_session()
@@ -9,9 +10,10 @@ session = setup_session()
 for patchline in patchlines:
     json_file = session.get(f"https://clientconfig.rpg.riotgames.com/api/v1/config/public?version=999.0.0.9999999&patchline={patchline}&app=Riot Client&namespace=keystone.self_update", timeout=2)
     json_file.raise_for_status()
+    json_data = json_file.json()
 
-    level = json_file.json()["keystone.self_update.level"]
-    manifest_url = json_file.json()["keystone.self_update.manifest_url"]
+    level = json_data["keystone.self_update.level"]
+    manifest_url = json_data["keystone.self_update.manifest_url"]
     os.makedirs(f"Riot Client/{patchline}", exist_ok=True)
     download_info = ("^Contents/Info.plist", "mac") if "Mac" in patchline else ("RiotClientFoundation.dll", "win")
     subprocess.check_call(["./ManifestDownloader.exe", manifest_url, "-b", "https://ks-foundation.secure.dyn.riotcdn.net/channels/public/bundles", "-f", download_info[0], "-o", "Riot Client/temp", "-t", "8"], timeout=10)
@@ -20,4 +22,4 @@ for patchline in patchlines:
     else:
         with open(f"Riot Client/temp/{download_info[0][1:]}", "rb") as in_file:
             exe_version = f'{plistlib.load(in_file)["FileVersion"]}_{manifest_url[-25:-9]}'
-    save_file(f"Riot Client/{patchline}/{exe_version}_{level}.json", json_file.content)
+    save_file(f"Riot Client/{patchline}/{exe_version}_{level}.json", json.dumps(json_data, sort_keys=True, separators=(',', ':')))
